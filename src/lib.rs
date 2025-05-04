8d708e133919ab3797dbd9446bb51b0a8a2b9d17
Florian Penn <uekue1@googlemail.com>
2025-05-04 14:38:14 +0200

    #4 Add crate documentation comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # BD18378
+//! A platform-agnostic Rust driver for the ROHM BD18378 LED Driver IC. 
+//! This driver is designed to be used with embedded systems and is compatible 
+//! with the `embedded-hal` crate.
+
 
 #[cfg(test)]
 mod tests {
