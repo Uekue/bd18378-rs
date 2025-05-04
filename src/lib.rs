97a856679047b0b123f0eea87696b4cf47c6c48e
Florian Penn <uekue1@googlemail.com>
2025-05-04 17:51:20 +0200

    #4 Make register module publicly available

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 //! # BD18378
-//! A platform-agnostic Rust driver for the ROHM BD18378 LED Driver IC. 
-//! This driver is designed to be used with embedded systems and is compatible 
+//! A platform-agnostic Rust driver for the ROHM BD18378 LED Driver IC.
+//! This driver is designed to be used with embedded systems and is compatible
 //! with the `embedded-hal` crate.
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod registers;
 
     #[test]
     fn it_works() {
