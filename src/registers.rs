1414206d14d84589ae01eb202953255850bcb5df
Florian Penn <uekue1@googlemail.com>
2025-05-04 12:19:09 +0200

    #2 Add reserved registers needed for init sequence

--- src/registers.rs
+++ src/registers.rs
@@ -2,5 +2,12 @@ pub enum WriteRegister {
     ChannelEnable00To05 = 0x56,
     ChannelEnable06To11 = 0x57,
     SoftwareReset = 0x6C,
-
+    Reserved79 = 0x79,
+    Reserved7A = 0x7A,
+    Reserved7B = 0x7B,
+    ReservedB5 = 0xB5,
+    ReservedB6 = 0xB6,
+    ReservedB7 = 0xB7,
+    ReservedB8 = 0xB8,
+    ReservedB9 = 0xB9,
 }
